ac2e3c9cdfd5e5ba62ec98663d61d330df1a37c0
AleGasMed <alejandromedina085@gmail.com>
2021-12-24 21:06:11 -0300

    completamos el ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -4,6 +4,43 @@ function mostrar()
 
 	var edadIngresada;
 
-	edadIngresada= document.getElementById('edad').value;
+	edadIngresada = document.getElementById('edad').value;
+	
+	/*if(edadIngresada >= 18) 
+	{
+		alert("Usted es mayor de edad");
+	}
+    if(edadIngresada <= 17)
+    {
+        alert("Usted es menor de edad");
+    }*/
+	
+	/*if(edadIngresada < 1)
+	{
+		alert("La edad no puede ser menor a 1");
+	}
+	else
+	{
+		if(edadIngresada >= 18)
+		{
+			alert("Usted es mayor de edad");
+		}
+		else 
+		{
+			alert("Usted es menor de edad");
+		}
+	}*/
 
-}//FIN DE LA FUNCIÓN
+	if(edadIngresada < 1)
+	{
+		alert("la edad no puede ser validada");
+	}
+	else if (edadIngresada > 17)
+	{
+		alert("Usted es mayor de edad");
+	}
+	else
+	{
+		alert("Usted es menor de edadS");
+	}
+}   //FIN DE LA FUNCIÓN
