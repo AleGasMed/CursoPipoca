6443911de68421fab98e989d36b93e1947b71d6a
AleGasMed <alejandromedina085@gmail.com>
2021-12-27 17:19:54 -0300

    Ejercicio del Do

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,3 +1,22 @@
 function mostrar()
 {
+    var clave;
+    clave = prompt("¿Cuál es la clave?");
+    while(clave != "utn750")
+    {
+        clave = prompt("Contraseña incorrecta");
+    }
+
+    /*do {
+
+        if (clave == undefined)
+        {
+            clave = prompt("Ingrese contraseña:");
+        } else {
+            clave = prompt("Incorrecto, ingrese contraseña:");
+        }
+
+    } while (clave != "utn750");*/
+    
+
 }//FIN DE LA FUNCIÓN
