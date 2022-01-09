18d0962c32011d5e543f4b92617e4a97ef9b2fc9
AleGasMed <alejandromedina085@gmail.com>
2022-01-09 11:24:59 -0300

    ejercicios del 9/1

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -8,25 +8,32 @@ function mostrar()
     var numeroPos = 0;
     var cero = 0;
     var numerosPares = 0;
+    var resta;
     var resto;
+    var promedioPos;
+    var promedioPos;
     var decision = "si";
 
     while(decision != "no")
     {
         numero = prompt("Coloque los numeros que desee");
         contador = contador + 1;
-
+    
         if(numero[0] == "-")
         {
+            
             sumaNeg = parseInt(numero) + sumaNeg;//suma los negativos
             numeroNeg = numeroNeg + 1;//cuenta cada negativo
+           
         }
-        else
+        else if(numero > 0)
         {
-            sumaPos = parseInt(numero) + sumaPos;//suma los negativos
+            sumaPos = parseInt(numero) + sumaPos;//suma los positivos;
             numeroPos = numeroPos + 1;
+            promedioPos = numero / numeroPos;
+           
         }
-
+             
         if(numero == 0)
         {
             cero = cero + 1;
@@ -37,27 +44,32 @@ function mostrar()
         if(resto == 0)
         {
             numerosPares = numerosPares + 1;
-            alert("HOLA " + numerosPares);
         }
         
+        do
+        {
+            decision= prompt("¿Desea continuar?");
+        }while(decision != "no" && decision != "si");
 
+        promedioNeg = contador / numeroNeg;
 
+        promedioPos = contador / numeroPos;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        decision = prompt("¿Desea continuar?");
+        
     }
+
+    resta = numeroPos - numeroNeg;
+    
+    document.write("La suma de los negativos es: " + sumaNeg + "<br>");
+    document.write("La suma de los positivos es: " + sumaPos + "<br>");
+    document.write("Cantidad de positivos: " + numeroPos + "<br>");
+    document.write("Cantidad de negativos: " + numeroNeg + "<br>");
+    document.write("Cantidad de ceros: " + cero + "<br>");
+    document.write("Cantidad de números pares: " + numerosPares + "<br>");
+    document.write("Promedio de positivos: " + promedioPos + "<br>");
+    document.write("Promedio de negativos: " + promedioNeg + "<br>");
+    document.write("Diferencia entre positivos y negativos: " + resta + "<br>");
+
 
 
     
--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -1,18 +1,58 @@
 /*2.	Para el departamento de Construcción:
-A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
-B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+A.	mostrar la cantidad de alambre a comprar si se ingresara el largo y el ancho de un terreno rectangular y se debe alambrar con tres hilos de alambre.
+B.	mostrar la cantidad de alambre a comprar si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+    var alambre;
+    var largo;
+    var ancho;
 
+    largo = document.getElementById("Largo").value;
+    ancho = document.getElementById("Ancho").value;
+
+    alambre = 2 * parseInt(largo) + 2 * parseInt(ancho);
+
+    alert("La cantidad de alambre que se debe comprar es de: " + alambre);
+
+    
 }
 function Circulo () 
 {
-	
+	var alambre;
+    var radio;
+
+    radio = document.getElementById("Radio").value;
+
+    alambre = 2 * 3.14 * parseInt(radio);
+
+    alert("La cantidad de alambre a comprar es: " + alambre);
 }
 function Materiales () 
 {
-	
+	var largo;
+    var ancho;
+    var bolsaDeCemento;
+    var bolsaDeCal;
+    var perimetroAlrectangulo;
+    var cuadrado;
+
+    ancho = document.getElementById("Largo").value;//lo importante es la altura en un cuadrado
+    largo = document.getElementById("Ancho").value;
+
+    perimetroAlrectangulo = 2 * parseInt(largo) + 2 * parseInt(ancho);
+    cuadrado = parseInt(largo) / parseInt(ancho);
+
+    bolsaDeCemento = cuadrado * 2;
+    bolsaDeCal = cuadrado * 3;
+
+    alert("Se necesitan " + bolsaDeCemento + " de cemento " + "\n" + "Se necesitan " + bolsaDeCal + " de cal");
+
+
+
+    
+
+    
 }
